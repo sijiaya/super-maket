49b6c91b83d2a91265e96ef129cf05960dffbf37
yaojia <2317321437@qq.com>
2018-12-24 13:46:26 +0800

    新增会员接口

--- src/main/java/com/soft1841/sm/dao/MemberDAO.java
+++ src/main/java/com/soft1841/sm/dao/MemberDAO.java
@@ -1,4 +1,38 @@
 package com.soft1841.sm.dao;
 
+import cn.hutool.db.Entity;
+
+import java.lang.reflect.Member;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface MemberDAO {
+    /**
+     * 新增会员
+     * @param member
+     * @return
+     */
+    Long insertMember(Member member) throws SQLException;
+
+    /**
+     * 删除会员
+     * @param number
+     * @return
+     */
+    int deleteMember(String number) throws SQLException;
+
+    /**
+     * 查询所有会员
+     * @return
+     * @throws SQLException
+     */
+    List<Entity> selectAllMember() throws SQLException;
+
+    /**
+     * 更新会员信息
+     * @param number
+     * @return
+     * @throws SQLException
+     */
+    int updateMember(String number) throws SQLException;
 }
--- src/main/java/com/soft1841/sm/dao/SellerDAO.java
+++ src/main/java/com/soft1841/sm/dao/SellerDAO.java
@@ -4,4 +4,5 @@ package com.soft1841.sm.dao;
  * 收银员的增删改查
  */
 public interface SellerDAO {
+
 }
