c85c7f304810b0e2fc3eb2d633045c630cd7a795
戴尔 <326513621@qq.com>
2018-12-29 13:34:55 +0800

    Merge branch 'master' of D:\jsuper-market with conflicts.

--- src/main/java/com/soft1841/sm/MainApp.java
+++ src/main/java/com/soft1841/sm/MainApp.java
@@ -9,12 +9,15 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/oldbackstage.fxml"));
-
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 2000, 1000));
-
-
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        //给舞台设置标题
+        primaryStage.setTitle("电脑管家");
+        //创建场景,并将舞步读入的布局文件的更节点加入其中
+        Scene scene = new Scene(root,1112,600);
+        //将外部的样式表读入
+        scene.getStylesheets().add(MainApp.class.getResource("/css/style.css").toExternalForm());
+        //场景加入舞台,并可见
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
